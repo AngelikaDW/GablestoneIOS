2c953feb18fd1c42f73fedce686cead9eee0f4ef
AngelikaDw <1ngelika>
2018-04-19 20:43:25 +0200

    with images

---
+++ GablestoneIOS/Views/Custom TableCell/TourTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TourTableViewCell.swift
+//  GablestoneIOS
+//
+//  Created by AngelikaDW on 19/04/2018.
+//  Copyright © 2018 aleaf. All rights reserved.
+//
+
+import UIKit
+
+class TourTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
